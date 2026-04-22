46762f10c2ecc001fa6a6ae018c7e67613522773
naya1228 <jin100y43@gmail.com>
2026-04-23 01:23:42 +0900

    RTC 틀 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import { useCallback, useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Button from "./components/Button";
+import { invoke } from "@tauri-apps/api/core";
+
+const RTC_CONFIG: RTCConfiguration = {
+  iceServers: [],
+};
+
+export function useIP() {
+  const [ip, setIP] = useState<string>("");
+  useEffect(() => {
+    invoke<string>("get_ip").then(setIP);
+  }, []);
+  return ip;
+}
 
 function App() {
-  const [ip, setIP] = useState("127.0.0.1");
-  const [connect_ip, set_arrive] = useState("");
-
-  const handleConnect = useCallback<React.SubmitEventHandler<HTMLFormElement>>(
-    (e) => {
-      e.preventDefault();
-      console.log(connect_ip);
-    },
-    [connect_ip],
-  );
+  const pcRef = useRef<RTCPeerConnection | null>(null);
+  const [signaling_server, set_server] = useState("http://localhost:6767");
 
   useEffect(() => {
-    invoke<string>("get_ip", {}).then(setIP);
+    return () => {
+      pcRef.current?.close();
+      pcRef.current = null;
+    };
   }, []);
+
+  const handleConnect = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const pc = new RTCPeerConnection(RTC_CONFIG);
+    pcRef.current = pc;
+    pc.onconnectionstatechange = () => {
+      if (pc.connectionState === "disconnected") {
+        pc.close();
+        pcRef.current = null;
+      }
+    };
+
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+  };
+
+  const handleOpenRoom = async () => {
+    if (pcRef.current === null) {
+    }
+  };
 
   return (
     <main className="flex flex-col bg-[#1F1F1E] items-center p-3 h-dvh">
@@ -25,13 +52,19 @@ function App() {
       <span className="text-white text-4xl font-bold m-2">
         SharingYourSounds
       </span>
-      <p className="m-3 text-gray-300">Current address is {ip}</p>
+      <div className="flex m-2 gap-2 items-center">
+        <p className="text-gray-500">your local ip is {useIP()}</p>
+        <Button type="button" onClick={handleOpenRoom}>
+          Open Host & Wait
+        </Button>
+      </div>
+      <span>or</span>
       <form className="flex gap-1.5 mb-10" onSubmit={handleConnect}>
         <input
-          onChange={(e) => set_arrive(e.target.value)}
+          onChange={(e) => set_server(`http://${e.target.value}:6767`)}
           className="outline rounded-md placeholder:text-gray-500 placeholder:italic"
           type="text"
-          placeholder="ex)192.168.43.1"
+          placeholder={"ex) " + useIP()}
         ></input>
         <Button type="submit">Connect</Button>
       </form>
